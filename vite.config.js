2511f72d41d81f5525c61ce2a1283a9bda0bfe65
Niki Mardari <nikitamardari@gmail.com>
2025-09-18 22:03:57 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/e-components-react/',
+  base: '/shopping-react/',
   plugins: [react()],
 })
+
 
